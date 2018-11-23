4ce56494955aeec81b478c6f331b83b2409794a2
gamirk <roshan.kumar202@gmail.com>
2018-11-23 05:46:09 -0500

    in progess

---
+++ quick_sort.py
@@ -0,0 +1,29 @@
+
+def quick(s, e):
+	pointer=s
+	print pointer,"->", li[pointer], e,"->",li[e]
+	for val in li[s:e-1]:
+		if li[e] > val:
+			ind=li.index(val)
+			print li[pointer], li[ind]
+			li[pointer],li[ind]=li[ind],li[pointer]
+			pointer=pointer+1
+	print "between", pointer, li[pointer]
+	li[pointer], li[e]=li[e], li[pointer]
+	print li
+	if s<pointer:
+		quick(s, pointer-1)
+	if pointer<e:
+		quick(pointer+1, e)
+
+def main():
+	print li
+#	val=li[6]
+#	pointer=0
+#	li[pointer], li[li.index(val)]=li[li.index(val)],li[pointer]
+	quick(0, len(li)-1)
+	print li
+
+if __name__ == "__main__":
+	li=[5,2,6,9,3,8,1,4,7]
+	main()
